7d3d17b393c3c92b3677db3b68137b99ab200717
kimsua <k021113a@naver.com>
2024-10-21 14:55:32 +0900

    수업 중

--- chap03-di-annotation/src/main/java/com/ohgiraffers/section01/subsection01/field/BookService.java
+++ chap03-di-annotation/src/main/java/com/ohgiraffers/section01/subsection01/field/BookService.java
@@ -1,11 +1,26 @@
 package com.ohgiraffers.section01.subsection01.field;
 
 
+import com.ohgiraffers.section01.common.BookDAO;
+import com.ohgiraffers.section01.common.BookDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service("bookServiceField")
 public class BookService {
 
+    // 인터페이스면 구현체가 있으면 되어있는 곳에 자동으로 넣음
+    @Autowired
+    private BookDAO bookDAO;
 
+    public List<BookDTO> selectAllBook() {
+        return bookDAO.selectBookList();
+    }
+
+    public BookDTO selectOneBook(int seq) {
+        return bookDAO.selectOneBook(seq);
+    }
 
 }
