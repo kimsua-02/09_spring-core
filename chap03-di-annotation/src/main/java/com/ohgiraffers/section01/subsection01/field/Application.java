79cede346189a50c0bd22236b33480bc9e4bea9b
kimsua <k021113a@naver.com>
2024-10-21 14:43:22 +0900

    수업 중

---
+++ chap03-di-annotation/src/main/java/com/ohgiraffers/section01/subsection01/field/Application.java
@@ -0,0 +1,4 @@
+package com.ohgiraffers.section01.subsection01.field;
+
+public class Application {
+}
---
+++ chap03-di-annotation/src/main/java/com/ohgiraffers/section01/subsection01/field/BookService.java
@@ -0,0 +1,11 @@
+package com.ohgiraffers.section01.subsection01.field;
+
+
+import org.springframework.stereotype.Service;
+
+@Service("bookServiceField")
+public class BookService {
+
+
+
+}
