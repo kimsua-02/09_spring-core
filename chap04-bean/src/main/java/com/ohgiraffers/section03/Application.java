9ba088dc59fbcbfe28b4d083b18371d1244cc1ab
kimsua <k021113a@naver.com>
2024-10-22 11:31:37 +0900

    수업 중

---
+++ chap04-bean/src/main/java/com/ohgiraffers/section03/Application.java
@@ -0,0 +1,40 @@
+package com.ohgiraffers.section03;
+
+import com.ohgiraffers.common.Beverage;
+import com.ohgiraffers.common.Bread;
+import com.ohgiraffers.common.ShoppingCart;
+import com.ohgiraffers.common.product;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class Application {
+    public static void main(String[] args) {
+
+        ApplicationContext context = new GenericXmlApplicationContext("section/xmlconfig/spring-config.xml");
+
+        String[] beanNames = context.getBeanDefinitionNames();
+
+
+        product carpBread = context.getBean("carpBread", Bread.class);
+        product milk = context.getBean("milk", Beverage.class);
+        product water = context.getBean("water", Beverage.class);
+
+        // 첫번째 손님이 쇼핑 카르를 꺼낸다.
+        ShoppingCart cart = context.getBean("cart", ShoppingCart.class);
+        cart.addItem(carpBread);
+        cart.addItem(milk);
+        System.out.println("cart 에 담긴 물품 : " + cart.getItems());
+
+        // 두 번 째 손님이 카트를 꺼낸다.
+        ShoppingCart cart2 = context.getBean("cart", ShoppingCart.class);
+        cart2.addItem(water);
+        System.out.println("cart2 에 담긴 물품 : " + cart2.getItems());
+
+        ((GenericXmlApplicationContext) context).close();
+
+
+
+    }
+
+}
